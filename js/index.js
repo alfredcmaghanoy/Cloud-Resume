02ecbd6778c2f591e3ea6e667cf3c5f846d3fa7a
Alfred Maghanoy <alfredcmaghanoy@gmail.com>
2025-11-16 21:44:30 +0800

    Initial Commit

---
+++ js/index.js
@@ -0,0 +1,20 @@
+/* Views counter
+    * ------------------------------------------------------ */ 
+    //Creates a variable to select an element from index.html file which is views-counter
+    const counter = document.querySelector(".views-counter"); 
+
+    // Defines an asynchronous function to fetch and update the counter
+    async function updateCounter () {        
+
+        // Sends a request to the AWS Lambda function URL and waits for the response. The URL is the aws lambda function url
+        let response = await fetch("https://hdgz2u5h4eanvfh735jpp6y6t40xtttb.lambda-url.ap-southeast-1.on.aws/"); 
+
+        // Converts the response into a JavaScript object and stores it in the "data" variable
+        let data = await response.json();      
+
+        // Updates the HTML content of the selected element with the retrieved view count     
+        counter.innerHTML = `    Views: <span class="views">  ${data}  </span>`; 
+    }
+
+    // Calls the function to execute when the script runs
+    updateCounter();
